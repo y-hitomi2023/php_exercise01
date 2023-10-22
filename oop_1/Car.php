35aede5caa12031cec405fadf24e632711239c8d
y-hitomi2023 <as8stream@gmail.com>
2023-10-22 16:46:31 +0900

    oop課題1完了

---
+++ oop_1/Car.php
@@ -0,0 +1,57 @@
+<?php
+
+class Car
+{
+
+    private $name;
+    private $number;
+    private $color;
+
+    public function __construct($name, $number, $color)
+    {
+        $this->name = $name;
+        $this->number = $number;
+        $this->color = $color;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function information()
+    {
+        return "車の車種:" .
+            "{$this->name}" . PHP_EOL .
+            "車体番号:" .
+            "{$this->number}" . PHP_EOL .
+            "カラー" .
+            "{$this->color}" . PHP_EOL;
+    }
+
+}
